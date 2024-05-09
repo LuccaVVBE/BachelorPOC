aa522497a8ce6238186334f322ba8d7c91882aa7
Lucca Van Veerdeghem <lucca.vanveerdeghem@student.hogent.be>
2024-05-09 16:05:45 +0200

    monolith

--- CI_Monolith/app/Config/Routes.php
+++ CI_Monolith/app/Config/Routes.php
@@ -5,4 +5,8 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'User::login');
+$routes->post('/', 'User::validate_login');
+$routes->get('/dashboard', 'Telephone::index');
+$routes->get('/usage', 'Call_Log::index');
+$routes->get('/logout', 'User::logout');
